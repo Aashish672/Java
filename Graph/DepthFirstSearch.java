aca99d0b40deb8e88db7576bb16b5d0431a0b5fc
Aashish Kumar Singh <aashishsinghjsr81@gmail.com>
2025-07-09 19:38:53 +0530

    Implemented Depth First Search Algorithm for Graph

---
+++ Graph/DepthFirstSearch.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class DepthFirstSearch {
+    public static void dfs(ArrayList<Edge> graph[],int curr,boolean vis[]){
+        System.out.print(curr+" ");
+        vis[curr]=true;
+
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            if(vis[e.dest]==false){
+                dfs(graph,e.dest,vis);
+            }
+            
+        }
+    }
+    public static void main(String[] args) {
+        int V=7;
+        ArrayList<Edge> graph[]=new ArrayList[V];
+        BreadthFirstSearch.createGraph(graph);
+        boolean vis[]=new boolean[V];
+
+        dfs(graph, 0, vis);
+        System.out.println();
+    }
+}
