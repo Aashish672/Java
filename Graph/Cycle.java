72c13bce6e7957d482467eeb66268a0bc7104bea
Aashish Kumar Singh <aashishsinghjsr81@gmail.com>
2025-07-14 17:44:13 +0530

    Implemented Cycle check algorithm for undirected graphs

--- Graph/Cycle.java
+++ Graph/Cycle.java
@@ -4,10 +4,27 @@ public class Cycle {
         for(int i=0;i<graph.length;i++){
             graph[i]=new ArrayList<>();
         }
-        graph[0].add(new Edge(0,2));
+        /**graph[0].add(new Edge(0,2));
         graph[1].add(new Edge(1,0));
         graph[2].add(new Edge(2,3));
-        graph[3].add(new Edge(3,0));
+        graph[3].add(new Edge(3,0));**/
+        graph[0].add(new Edge(0,1));
+        graph[0].add(new Edge(0,4));
+
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,4));
+
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,3));
+        
+        graph[3].add(new Edge(3,2));
+
+        graph[4].add(new Edge(4,0));
+        graph[4].add(new Edge(4,1));
+        graph[4].add(new Edge(4,5));
+
+        graph[5].add(new Edge(5,4));
     }
     public static boolean isCycleDirected(ArrayList<Edge> graph[],boolean vis[],int curr,boolean rec[]){
         vis[curr]=true;
@@ -25,12 +42,27 @@ public class Cycle {
         rec[curr]=false;
         return false;
     }
+
+    public static boolean isCycleUndirected(ArrayList<Edge> graph[],boolean vis[],int curr,int par){
+        vis[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            if(vis[e.dest] && e.dest!=par){
+                return true;
+            }else if(!vis[e.dest]){
+                if(isCycleUndirected(graph, vis, e.dest, curr)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        int V=4;
+        int V=6;
         ArrayList<Edge> graph[]=new ArrayList[V];
         createGraph(graph);
 
-        boolean vis[]=new boolean[V];
+        /**boolean vis[]=new boolean[V];
         boolean rec[]=new boolean[V];
         for(int i=0;i<V;i++){
             if(!vis[i]){
@@ -41,6 +73,8 @@ public class Cycle {
                 }
             }
         }
-        //System.out.println(isCycleDirected(graph, new boolean[V], 0, new boolean[V]));
+        //System.out.println(isCycleDirected(graph, new boolean[V], 0, new boolean[V]));**/
+
+        System.out.println(isCycleUndirected(graph,new boolean[V], 0, -1));
     }
 }
